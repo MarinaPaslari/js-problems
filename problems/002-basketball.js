670b948c68315daf19420fa758f0e854258d03d0
Marina <marina.paslari@gmail.com>
2021-01-31 16:30:38 +0200

    second

--- problems/002-basketball.js
+++ problems/002-basketball.js
@@ -1,9 +1,11 @@
 /**
- * Известны результаты каждой из 4х четвертей баскетбольной встречи.
+ * Известны результаты каждой из 4х четвертей баскетбольной
+ *  встречи.
  * Нужно определить победителя матча. Побеждает команда,
  * набравшая больше очков в течение всего матча.
  *
- * Напишите функцию getWinner(points) возвращающую номер победившей команды,
+ * Напишите функцию getWinner(points) возвращающую номер 
+ * победившей команды,
  * либо undefined в случае ничьей.
  *
  * Пример:
@@ -16,7 +18,21 @@
  * @returns {(number|undefined)}
  */
 function getWinner(points) {
-    return undefined;
+    let sumPoint1 = 0,
+        sumPoint2 = 0;
+    for(let i=0; i<points.length; i++) {
+        let arr = points[i].split('-');
+        sumPoint1 += (+arr[0]);
+        sumPoint2 += (+arr[1]);
+    }
+
+    if(sumPoint1 > sumPoint2) {
+       return 1;
+    } else if(sumPoint1 < sumPoint2){
+        return 2;
+    } else {
+        return undefined;
+    }
 }
 
 module.exports = getWinner;
