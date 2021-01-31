f1716a565be07937aa5065f15301361d9de545d9
Marina <marina.paslari@gmail.com>
2021-01-31 12:23:35 +0200

    first

--- problems/001-sum.js
+++ problems/001-sum.js
@@ -1,5 +1,6 @@
 /**
- * Напишите функцию sum(n) возвращающую положительное целое число – сумму чисел,
+ * Напишите функцию sum(n) возвращающую положительное целое
+ *  число – сумму чисел,
  * расположенных между 1 и N включительно.
  *
  * Пример:
@@ -12,8 +13,17 @@
  * @param {number} n целое число
  * @returns {number}
  */
+
 function sum(n) {
-    return undefined;
+    if(n<=0){
+        return 1;
+    } 
+
+    let summ = 0;
+    for (let i=1; i<=n; i++) {
+        summ = summ + i;
+    } 
+    return summ;
 }
 
 module.exports = sum;
